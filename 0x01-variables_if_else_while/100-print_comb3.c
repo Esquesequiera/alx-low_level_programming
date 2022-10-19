5f517d81f4d649a59792b090b207d90f2e9619e0
Esquesequiera <seqimogereh@gmail.com>
2022-10-19 08:28:38 -0700

    diff combinations btwn two different digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,10 +14,12 @@ int main(void)
 		{
 			putchar((x % 10) + '0');
 			putchar((y % 10) + '0');
-		}
-		if (x == 9 && y == 8)
+
+			if (x == 9 && y == 9)
+				continue;
 			putchar(',');
-		putchar(' ');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
