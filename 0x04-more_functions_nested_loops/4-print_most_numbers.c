6b36ff0c929eb7dd2565bf5a47de732c335f9839
Esquesequiera <seqimogereh@gmail.com>
2022-10-21 06:32:46 -0700

    function that prints numbers from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,9 @@ void print_most_numbers(void)
 
 	while (a < 10)
 	{
-		if (a != 0 && a != 9)
-			_putchar(a++ + '0')
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+		a++;
 	}
 	_putchar('\n');
 }
