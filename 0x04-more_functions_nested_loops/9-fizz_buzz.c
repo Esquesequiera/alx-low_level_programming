7208cb6434fee901568c435ff6d9daef64c60096
Esquesequiera <seqimogereh@gmail.com>
2022-10-23 23:41:44 -0700

    Fizz Buzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,18 +8,17 @@
 
 int main(void)
 {
-	int (n);
-
 	for (n == 1; n <= 100; n++)
 	{
 		printf(" ");
-		if (n % 3 == 0)
-			printf("Fizz");
-		if (n % 5 == 0)
-			printf("Buzz");
-		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz");
+		if ((n % 3 == 0) && (n % 5 == 0))
+			printf("FIzz Buzz\n");
+		else if (n % 3 == 0)
+			printf("Fizz\n");
+		else if (n % 5 == 0)
+			printf("Buzz\n");
+		else
+			printf("%d\n", n);
 	}
-	printf("\n");
 	return (0);
 }
