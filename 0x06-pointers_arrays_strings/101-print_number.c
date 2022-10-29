d2a4469006d23c61fe933199df206299fe231e2b
Esquesequiera <seqimogereh@gmail.com>
2022-10-28 17:42:36 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -14,8 +14,8 @@ void print_number(int n)
 		_putchar('-');
 		num = -num;
 	}
-	if ((n / 10) > 0)
-		_putchar(num / 10);
+	if ((num / 10) > 0)
+		print_number(num / 10);
 
 	_putchar((num % 10) + '0');
 }
